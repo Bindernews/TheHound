5c56965b2333158f3be62c6eeb879c8ffa2cc646
KWBrown <brownkmod2@gmail.com>
2017-04-15 03:02:31 -0400

    Update image_identifier.py

--- identifiers/image_identifier.py
+++ identifiers/image_identifier.py
@@ -17,6 +17,12 @@ PNG_PATTERNS = [
 	'89 50 4E 47'
 ]
 
+BMP_PATTERNS = [
+	'42 4D 62 25',
+	'42 4D F8 A9',
+	'42 4D 76 02',
+]
+
 class PngResolver:
 	def identify(self, stream):
 		return Result('PNG')
@@ -29,6 +35,10 @@ class GifResolver:
 	def identify(self, stream):
 		return Result('GIF')
 
+class BmpResolver:
+	def identify(self, stream):
+		return Result('BMP')
+	
 def load(hound):
 	# Register JPEGs
 	hound.add_matches(JPEG_PATTERNS, JpegResolver())
@@ -36,3 +46,5 @@ def load(hound):
 	hound.add_matches(PNG_PATTERNS, PngResolver())
 	# Register GIFs
 	hound.add_matches(GIF_PATTERNS, GifResolver())
+	# Register BMPs
+	hound.add_matches(BMP_PATTERNS, BmpResolver())
