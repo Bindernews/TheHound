82200944672fe8a08c712e83e661f9b8b197adbe
KWBrown <brownkmod2@gmail.com>
2017-04-17 14:20:34 -0400

    Update audio_identifier.py

--- identifiers/audio_identifier.py
+++ identifiers/audio_identifier.py
@@ -1,12 +1,22 @@
 from identifier import Result
 	
 AU_PATTERNS = [
-	'2E 72 6E 64'
+	'2E 72 6E 64',
+	
+]
+
+AIFF_PATTERNS = [
+	'46 4F 52 4D 00'
 ]
 	
 class AuResolver:
 	def identify(self, stream):
 		return Result('AU')
+
+class AiffResolver:
+	def identify(self, stream):
+		return Result('AIFF')
 	
 def load(hound):
 	hound.add_matches(AU_PATTERNS, AuResolver())
+	hound.add_matches(AIFF_PATTERNS, AiffResolver())
