e0b3faf797020b1b37f6eae0a465d76490a83d63
Drew H <bindernews@gmail.com>
2017-04-21 17:15:57 -0400

    Merge ELF identifier into executable identifier and try to identify mp3 files.

--- identifiers/audio_identifier.py
+++ identifiers/audio_identifier.py
@@ -1,22 +1,45 @@
 from identifier import Result
-	
+
+MP3_DESC = 'MP3 Audio'
+
 AU_PATTERNS = [
-	'2E 72 6E 64',
-	
+    '2E 72 6E 64',
 ]
+
+MP3_PATTERNS = set([
+    bytes.fromhex('FF FB'),
+    bytes.fromhex('FF FA'),
+])
 
 AIFF_PATTERNS = [
-	'46 4F 52 4D 00'
+    '46 4F 52 4D 00'
 ]
-	
+
+class BadMp3Error(BaseException):
+    def __init__(*args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+class Mp3Resolver:
+    def read_frame(self, stream):
+        header = stream.read(4)
+        if not header[0:2] in MP3_PATTERNS:
+            return False
+        has_crc = (header[1] & 8) == 0
+        bitrate_index = header[2] & 0x0F
+
+    def identify(self, stream):
+        return Result('MP3', MP3_DESC)
+        # if not self.read_frame(stream):
+        #     return Result('MP3', MP3_DESC)
+    
 class AuResolver:
-	def identify(self, stream):
-		return Result('AU')
+    def identify(self, stream):
+        return Result('AU')
 
 class AiffResolver:
-	def identify(self, stream):
-		return Result('AIFF')
-	
+    def identify(self, stream):
+        return Result('AIFF')
+    
 def load(hound):
-	hound.add_matches(AU_PATTERNS, AuResolver())
-	hound.add_matches(AIFF_PATTERNS, AiffResolver())
+    hound.add_matches(AU_PATTERNS, AuResolver())
+    hound.add_matches(AIFF_PATTERNS, AiffResolver())
--- identifiers/executable_identifier.py
+++ identifiers/executable_identifier.py
@@ -5,7 +5,7 @@ EXE_PATTERNS = [
   '4D 5A 90 00',
 ]
 
-BIN_PATTERNS = [
+ELF_PATTERNS = [
     '7F 45 4C 46'
 ]
 
@@ -13,9 +13,9 @@ MSI_PATTERNS = [
     'D0 CF 11 E0'
 ]
 
-class BinResolver:
+class ElfResolver:
     def identify(self, stream):
-        return Result('BIN', 'Linux executable')
+        return Result('ELF', 'Executable and Linkable Format (Linux binaries)')
     
 class ExeResolver:
     def identify(self, stream):
@@ -28,5 +28,5 @@ class MsiResolver:
 
 def load(hound):
     hound.add_matches(EXE_PATTERNS, ExeResolver())
-    hound.add_matches(BIN_PATTERNS, BinResolver())
+    hound.add_matches(ELF_PATTERNS, BinResolver())
     hound.add_matches(MSI_PATTERNS, MsiResolver())
